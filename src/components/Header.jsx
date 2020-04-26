3f6552c478219dd703d69398a0f09fbfc1ca47d0
Zaenal Mustofa <43901015+zaenalmusthofa86@users.noreply.github.com>
2020-04-26 22:32:32 +0700

    Membuat button dengan CSS module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Button from "../components/Button";
+import Button from "../components/button/Button";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
