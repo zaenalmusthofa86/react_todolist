1d9707f68ba48a09c0f26abd4b2a166eff392a52
Zaenal Mustofa <43901015+zaenalmusthofa86@users.noreply.github.com>
2020-04-26 17:42:36 +0700

    Membuat Button Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Button from "../components/Button";
+
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
     <section className="header">
-      <button className="header-btn male-black-color" onClick={showAddToggle}>
+      {/* <button className="header-btn male-black-color" onClick={showAddToggle}>
         {showAdd ? "finish" : "Add"}
-      </button>
+      </button> */}
+      <Button text={showAdd ? "finish" : "Add"} onClick={showAddToggle} />
       <h1 className="header-title">Todo Lists</h1>
-      <button className="header-btn male-red-color" onClick={clearTodos}>
+      {/* <button className="header-btn male-red-color" onClick={clearTodos}>
         Clear
-      </button>
+      </button> */}
+      <Button text="Clear" onClick={clearTodos} color="red" align="right" />
     </section>
   );
 };
