a4fb0ab7575a7a5f01e6d10a979f31af7c7e9ee9
Zaenal Mustofa <43901015+zaenalmusthofa86@users.noreply.github.com>
2020-04-28 22:43:14 +0700

    Membuat Emotion CSS Props Header Components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+/**@jsx jsx */
+// import React from "react";
+import { jsx } from "@emotion/core";
 import PropTypes from "prop-types";
 
 import Button from "../button/Button";
 
-import styles from "./header.module.css";
+// import styles from "./header.module.css";
+import * as styles from "./header.styles";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
-    <section className={styles.header}>
+    <section css={styles.header}>
       {/* <button className="header-btn male-black-color" onClick={showAddToggle}>
         {showAdd ? "finish" : "Add"}
       </button> */}
       <Button text={showAdd ? "finish" : "Add"} onClick={showAddToggle} />
-      <h1 className={styles.headerTitle}>Todo Lists</h1>
+      <h1 css={styles.headerTitle}>Todo Lists</h1>
       {/* <button className="header-btn male-red-color" onClick={clearTodos}>
         Clear
       </button> */}
